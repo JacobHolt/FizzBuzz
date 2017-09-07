9dfce928cfa4ba19431b7249422ebed5cd68988a
Jacob Holt <levi_holt@yahoo.com>
2017-09-06 23:20:01 -0500

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class FizzBuzz {
+	public static void main(String [] args){
+		for(int i = 0; i <= 100; i++) {
+			if(i%3==0 && i%5==0) {
+				System.out.println(i + "FizzBuzz");
+			}
+			else if(i%5==0){
+				System.out.println(i + "Buzz");
+			}
+			else if(i%3==0){
+				System.out.println(i + "Fizz");
+			}
+			else {
+				System.out.print(i + "\n");	
+			}
+		}
+	}
+}
